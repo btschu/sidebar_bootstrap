9ba77f8c4e7f8fd8c35d4e401d3e1d5e9a4b2c69
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2024-02-21 10:27:04 -0700

    mobile responsive

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const hamburgerMenu = document.querySelector(".toggle-btn");
 const sideBar = document.querySelector("#sidebar");
+const sideBarItem = document.querySelectorAll(".sidebar-item");
+const sideBarFooter = document.querySelectorAll(".sidebar-footer");
 const logo = document.querySelector(".logo");
+const toggleButton = document.querySelector(".toggle-btn");
 const sideBarHover = document.querySelectorAll("[data-hover]");
 
 hamburgerMenu.addEventListener("click", function () {
-  sideBar.classList.toggle("expand"); // expands sidebar menu
-  logo.classList.toggle("expand"); // hides logo in main and adds it to the sidebar
+  sideBar.classList.toggle("expand");
+  logo.classList.toggle("expand");
+  toggleButton.classList.toggle("expand");
   for (i = 0; i < sideBarHover.length; i++) {
-    console.log(sideBarHover[i]);
     sideBarHover[i].classList.toggle("expand");
+  }
+  for (i = 0; i < sideBarItem.length; i++) {
+    sideBarItem[i].classList.toggle("expand");
+  }
+  for (i = 0; i < sideBarFooter.length; i++) {
+    sideBarFooter[i].classList.toggle("expand");
   }
 });
